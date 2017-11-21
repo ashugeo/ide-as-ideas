1c5778b9d8677d057554384eba0fd6d2ba898549
Ashugeo <hugo.saintemarie@gmail.com>
2017-11-21 12:49:26 +0100

    sidenotes management

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 $(document).ready(() => {
-    $('.stackedit__html').sidenotes();
+    $('a[href^="#fn"]').each((id, el) => {
+        const $el = $(el);
+        const index = id + 1;
+        $el.html(index);
+        $el.attr('href', '#fn' + index);
+        if (!$el.hasClass('footnote-backref')) {
+            const href = $el.attr('href');
+            const $note = $(href);
+            $el.parent().after(`<div id="${href.replace('#', '')}" class="sidenote"><a class="footnote-backref" href="#fnref${index}">${index}</a>${$note.html()}</div>`);
+        }
+    });
+
+    $('.footnotes-sep').hide();
+    $('.footnotes').hide();
 });

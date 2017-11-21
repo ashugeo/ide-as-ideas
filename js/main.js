198aa55d56eab38a7fa912d85adcc6fe6ec5671f
Ashugeo <hugo.saintemarie@gmail.com>
2017-11-21 16:30:19 +0100

    References management

--- js/main.js
+++ js/main.js
@@ -1,5 +1,41 @@
+let refs = {};
+
 $(document).ready(() => {
+    // Load references
+    $.getJSON('../refs.json', (data) => {
+        refs = data;
+        references();
+        sideNotes();
+    });
+});
+
+function references() {
+    // Parse references
+    $('cite').each((id, el) => {
+        $el = $(el);
+        // Get reference ID
+        const text = $el.text();
+        const refID = text.split(':')[0];
+
+        // Get reference object
+        const ref = refs[refID];
+
+        // Get passage
+        let passage = text.split(':')[1];
+
+        let citation = `${ref.author}, <i>${ref.title}</i> (${ref.year})`;
+        if (passage) {
+            citation += `, p.&nbsp;${passage}`;
+        }
+
+        $el.html(citation);
+    });
+}
+
+function sideNotes() {
+    // Parse sidenotes
     let index = 1;
+
     $('p').each((pId, pEl) => {
         const $pEl = $(pEl);
         let notesWide = '';
@@ -13,10 +49,10 @@ $(document).ready(() => {
                 const href = $aEl.attr('href');
                 const $note = $(href);
                 notesWide += `<div id="${href.replace('#', '')}" class="sidenote sidenote-wide">
-                    <a class="footnote-backref" href="#fnref${index}">${index}</a>
+                <a class="footnote-backref" href="#fnref${index}">${index}</a>
                 ${$note.html()}</div>`;
                 notesSmall += `<div id="${href.replace('#', '')}" class="sidenote sidenote-small">
-                    <a class="footnote-backref" href="#fnref${index}">${index}</a>
+                <a class="footnote-backref" href="#fnref${index}">${index}</a>
                 ${$note.html()}</div>`;
             }
             index += 1;
@@ -26,6 +62,7 @@ $(document).ready(() => {
         $pEl.after(notesSmall);
     });
 
+    // Hide footnotes
     $('.footnotes-sep').hide();
     $('.footnotes').hide();
-});
+}

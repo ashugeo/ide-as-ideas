d6e570f751dc7fcc00d5725011d3123b3557be45
Ashugeo <hugo.saintemarie@gmail.com>
2017-11-21 15:47:00 +0100

    Awful double sidenotes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,29 @@
 $(document).ready(() => {
-    $('a[href^="#fn"]').each((id, el) => {
-        const $el = $(el);
-        const index = id + 1;
-        $el.html(index);
-        $el.attr('href', '#fn' + index);
-        if (!$el.hasClass('footnote-backref')) {
-            const href = $el.attr('href');
-            const $note = $(href);
-            $el.closest('p').after(`<div id="${href.replace('#', '')}" class="sidenote"><a class="footnote-backref" href="#fnref${index}">${index}</a>${$note.html()}</div>`);
-        }
+    let index = 1;
+    $('p').each((pId, pEl) => {
+        const $pEl = $(pEl);
+        let notesWide = '';
+        let notesSmall = '';
+
+        $pEl.find('a[href^="#fn"]').each((aId, aEl) => {
+            const $aEl = $(aEl);
+            $aEl.html(index);
+            $aEl.attr('href', '#fn' + index);
+            if (!$aEl.hasClass('footnote-backref')) {
+                const href = $aEl.attr('href');
+                const $note = $(href);
+                notesWide += `<div id="${href.replace('#', '')}" class="sidenote sidenote-wide">
+                    <a class="footnote-backref" href="#fnref${index}">${index}</a>
+                ${$note.html()}</div>`;
+                notesSmall += `<div id="${href.replace('#', '')}" class="sidenote sidenote-small">
+                    <a class="footnote-backref" href="#fnref${index}">${index}</a>
+                ${$note.html()}</div>`;
+            }
+            index += 1;
+        });
+
+        $pEl.before(notesWide);
+        $pEl.after(notesSmall);
     });
 
     $('.footnotes-sep').hide();
